d35ed62d33f28aeaf31cacdbdffbe1246c905c97
baladitya dhaka <dhakabaladitya@gmail.com>
2025-04-11 02:01:24 +0530

    initial-setup-data-and-packages

---
+++ src/AuctionService/Entites/Auction.cs
@@ -0,0 +1,22 @@
+﻿namespace AuctionService.Entites
+{
+    public class Auction
+    {
+        public Guid Id { get; set; }
+        public int ReservePrice { get; set; } = 0;
+        public string? Seller {  get; set; }
+        public string? Winner { get; set; }
+        public int SoldAmount { get; set; }
+        public int CurrentHighBid { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime UpdatedAt{ get; set; } = DateTime.Now;
+        public DateTime AuctionEnd{ get; set; }
+
+        public Status? status { get; set; }
+
+        public Item? Item { get; set; }
+
+
+
+    }
+}
---
+++ src/AuctionService/Entites/Item.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.VisualBasic;
+
+namespace AuctionService.Entites
+{
+    [Table("Items")]
+    public class Item
+    {
+        public Guid Id { get; set; }
+        public string? Make { get; set; }
+        public string? Model { get; set; }
+        public int year { get; set; }
+        public string? color { get; set; }
+
+        public int Mileage { get; set; }
+
+        public string? ImgUrl { get; set; }
+        public Auction? Auction { get; set; }
+        public Guid AuctionId {  get; set; }
+
+
+
+    }
+}
